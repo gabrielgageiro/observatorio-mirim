70c0acd9a7d30b46ffbe1e42747dca2a6a890338
gabrielgageiro <gabrielgageiro@gmail.com>
2019-09-13 13:13:36 -0300

    adicionado os listeners

--- app/src/main/java/com/example/observatorio_mirim/MainActivity.java
+++ app/src/main/java/com/example/observatorio_mirim/MainActivity.java
@@ -1,21 +1,37 @@
 package com.example.observatorio_mirim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-
-public class MainActivity extends AppCompatActivity {
-
-    private BottomNavigationView navigationView;
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, BottomNavigationView.OnNavigationItemReselectedListener {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_bar);
 
-        navigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        initBottomNavigationBar();
+    }
+
+    private void initBottomNavigationBar(){
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setOnNavigationItemReselectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
+    }
+
+    @Override
+    public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+
     }
 }
