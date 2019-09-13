b012c7dd7995012186c4bc7314c09b5dbe8e66f9
gabrielgageiro <gabrielgageiro@gmail.com>
2019-09-12 21:40:33 -0300

    gitinore

--- app/src/main/java/com/example/observatorio_mirim/MainActivity.java
+++ app/src/main/java/com/example/observatorio_mirim/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.observatorio_mirim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationView = (BottomNavigationView) findViewById(R.id.navigationView);
     }
 }
