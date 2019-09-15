73fe85e595b08cbb4461d72297140cd59740eefd
cassioelias1 <cassio_qwe@outlook.com>
2019-09-15 00:01:10 -0300

    Instânciando o botão login.

--- app/src/main/java/com/example/observatorio_mirim/LoginActivity.java
+++ app/src/main/java/com/example/observatorio_mirim/LoginActivity.java
@@ -3,12 +3,16 @@ package com.example.observatorio_mirim;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btn_login);
     }
 }
