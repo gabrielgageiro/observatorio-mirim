75cb3ab79632423f0412446fd7d79a3837cdd7c0
cassioelias1 <cassio_qwe@outlook.com>
2019-11-15 22:46:55 -0300

    Implementando as animações na tela de login para informar as ações realizadas.

--- app/src/main/java/com/observatorioMirim/LoginActivity.java
+++ app/src/main/java/com/observatorioMirim/LoginActivity.java
@@ -20,6 +20,7 @@ import com.observatorioMirim.api.models.entrada.EntradaItem;
 import com.observatorioMirim.api.models.escola.Escola;
 import com.observatorioMirim.api.models.fornecedor.Fornecedor;
 import com.observatorioMirim.api.models.produto.Produto;
+import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
 
 import java.sql.Date;
 import java.time.LocalDate;
@@ -68,7 +69,15 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(Objects.requireNonNull(edit_text_usuario.getText()).length() == 0 && Objects.requireNonNull(edit_text_senha.getText()).length() == 0){
-                    System.out.println("Implementar Animação informando que o usuário e a senha devem ser preenchidos");
+                    new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.WARNING_TYPE)
+                            .setTitleText("Atenção!")
+                            .setContentText("O usuário e a senha devem ser preenchidos")
+                            .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                @Override
+                                public void onClick(SweetAlertDialog sweetAlertDialog) {
+                                    sweetAlertDialog.dismissWithAnimation();
+                                }
+                            }).show();
                     return;
                 }
 
@@ -80,13 +89,28 @@ public class LoginActivity extends AppCompatActivity {
                         if(response != null && response.body() != null){
                             if(response.body().getUsuario_smart().equals(escola.getUsuario_smart()) && response.body().getSenha_smart().equals(escola.getSenha_smart())){
                                 //TODO: Criar classe observatorioUtils, salvar as chaves aqui, salvar o idConta e idEscola no share
-
+                                /*new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.SUCCESS_TYPE)
+                                        .setTitleText("Bem Vindo")
+                                        .setContentText("WELCOME")
+                                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                            @Override
+                                            public void onClick(SweetAlertDialog sweetAlertDialog) {
+                                                sweetAlertDialog.dismissWithAnimation();
+                                            }
+                                        }).show();*/
                                 Intent it = new Intent(LoginActivity.this, MainActivity.class);
                                 startActivity(it);
-                                //TODO: Implementar uma mensagem informando que o login foi efetivado ou Bem Vindo!
                             }
                         } else {
-                            System.out.println("Erro ao encontrar a escola");
+                            new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.ERROR_TYPE)
+                                    .setTitleText("Atenção!")
+                                    .setContentText("O usuário ou senha informados estão incorretoses")
+                                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                        @Override
+                                        public void onClick(SweetAlertDialog sweetAlertDialog) {
+                                            sweetAlertDialog.dismissWithAnimation();
+                                        }
+                                    }).show();
                         }
                     }
 
@@ -97,212 +121,5 @@ public class LoginActivity extends AppCompatActivity {
                 });
             }
         });
-
-        //incluirProduto();
-        //listarProdutos();
-        //listarProdutoByNome();
-        //listarProdutoById();
-        //listarProdutoByCodigoBarras();
-
-        //incluirFornecedor();
-        //listarFornecedores();
-
-        //incluirEntrada();
-        incluirAluno();
-        //incluirItem();
     }
-
-    private void incluirItem() {
-        final EntradaItem entradaItem = new EntradaItem();
-        entradaItem.setIdConta(10);
-        entradaItem.setIdEscola(9);
-        entradaItem.setIdFornecedor(3);
-        entradaItem.setIdProduto(3);
-        entradaItem.setObservacao("Eaii tudo bom?");
-
-
-        API.postEntradaItem(entradaItem, new Callback<RespostaEscola>() {
-            @Override
-            public void onResponse(Call<RespostaEscola> call, Response<RespostaEscola> response) {
-                System.out.println(response.body().getMensagem());
-                System.out.println(response.body().getId());
-            }
-
-            @Override
-            public void onFailure(Call<RespostaEscola> call, Throwable t) {
-                System.out.println("Deu erro");
-            }
-        });
-    }
-
-    private void incluirEntrada() {
-        final Entrada entradas = new Entrada();
-        entradas.setIdConta(10);
-        entradas.setIdEscola(9);
-        entradas.setObservacao("Eaii tudo bom?");
-//        entradas.setDataHora(LocalDateTime.now());
-        entradas.setEntradaAlunos(new ArrayList<EntradaAluno>());
-        entradas.setEntradaItems(new ArrayList<EntradaItem>());
-
-
-        API.postEntrada(entradas, new Callback<RespostaEscola>() {
-            @Override
-            public void onResponse(Call<RespostaEscola> call, Response<RespostaEscola> response) {
-                System.out.println(response.body());
-            }
-
-            @Override
-            public void onFailure(Call<RespostaEscola> call, Throwable t) {
-                System.out.println("Deu erro");
-            }
-        });
-    }
-
-    private void incluirAluno() {
-        final EntradaAluno entradaAluno = new EntradaAluno();
-        entradaAluno.setIdConta(10);
-        entradaAluno.setIdEscola(9);
-        entradaAluno.setNomeAluno("Cássio 3.0");
-
-        API.postEntradaAluno(entradaAluno, new Callback<RespostaEscola>() {
-            @Override
-            public void onResponse(Call<RespostaEscola> call, Response<RespostaEscola> response) {
-                System.out.println(response.body().getMensagem());
-                System.out.println(response.body().getId());
-            }
-
-            @Override
-            public void onFailure(Call<RespostaEscola> call, Throwable t) {
-                System.out.println("Deu erro");
-            }
-        });
-
-    }
-
-
-    private void listarProdutoByCodigoBarras() {
-        API.getProdutoByCodigoBarras(10, 9, "7891149102488", new Callback<Produto>() {
-            @Override
-            public void onResponse(Call<Produto> call, Response<Produto> response) {
-                System.out.println(response.body().getNome());
-            }
-
-            @Override
-            public void onFailure(Call<Produto> call, Throwable t) {
-                System.out.println("Deu erro");
-            }
-        });
-    }
-
-    private void listarProdutoById() {
-        API.getProdutoById(10, 9, 2, new Callback<Produto>() {
-            @Override
-            public void onResponse(Call<Produto> call, Response<Produto> response) {
-                System.out.println(response.body().getNome());
-            }
-
-            @Override
-            public void onFailure(Call<Produto> call, Throwable t) {
-                System.out.println("Deu erro");
-            }
-        });
-    }
-
-    private void listarProdutoByNome() {
-        API.getProdutoByNome(10, 9, "Leite Milnutri", new Callback<Produto>() {
-            @Override
-            public void onResponse(Call<Produto> call, Response<Produto> response) {
-                System.out.println(response.body().getNome());
-            }
-
-            @Override
-            public void onFailure(Call<Produto> call, Throwable t) {
-                System.out.println("Deu erro");
-            }
-        });
-    }
-
-
-    private void listarFornecedores() {
-        API.getFornecedores(10, 9, new Callback<List<Fornecedor>>() {
-            @Override
-            public void onResponse(Call<List<Fornecedor>> call, Response<List<Fornecedor>> response) {
-                if(!response.body().isEmpty()) {
-                    for (Fornecedor fornecedor : response.body()) {
-                        System.out.println(fornecedor.getNome());
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Fornecedor>> call, Throwable t) {
-            }
-        });
-    }
-
-    private void incluirFornecedor() {
-        final Fornecedor[] fornecedores = new Fornecedor[1];
-        fornecedores[0] = new Fornecedor();
-        fornecedores[0].setIdConta(10);
-        fornecedores[0].setIdEscola(9);
-        fornecedores[0].setNome("Dani Boy");
-        fornecedores[0].setCnpj("184145454554");
-        fornecedores[0].setTelefone("489947455");
-
-        API.postFornecedor(fornecedores, new Callback<RespostaEscola>() {
-            @Override
-            public void onResponse(Call<RespostaEscola> call, Response<RespostaEscola> response) {
-                System.out.println(response.body().getMensagem());
-            }
-
-            @Override
-            public void onFailure(Call<RespostaEscola> call, Throwable t) {
-
-            }
-        });
-    }
-
-    /*private void listarProdutos() {
-        API.getProdutos(10, 9, new Callback<List<Produto>>() {
-            @Override
-            public void onResponse(Call<List<Produto>> call, Response<List<Produto>> response) {
-               if(!response.body().isEmpty()){
-                   for (Produto p : response.body()) {
-                       System.out.println(p.getNome());
-                   }
-               }
-            }
-
-            @Override
-            public void onFailure(Call<List<Produto>> call, Throwable t) {
-                t.printStackTrace();
-            }
-        });
-    }*/
-
-    /*private void incluirProduto() {
-        final Produto[] produtos = new Produto[1];
-        produtos[0] = new Produto();
-        produtos[0].setIdConta(10);
-        produtos[0].setIdEscola(9);
-        produtos[0].setNome("Lança Perfume");
-        produtos[0].setMarca("Lança");
-        produtos[0].setUnidade("ML");
-        produtos[0].setCodigoBarras("1263531253");
-
-
-        API.postProdutos(produtos, new Callback<RespostaEscola>() {
-            @Override
-            public void onResponse(Call<RespostaEscola> call, Response<RespostaEscola> response) {
-                System.out.println(response.body().getMensagem());
-            }
-
-            @Override
-            public void onFailure(Call<RespostaEscola> call, Throwable t) {
-
-            }
-        });
-    }*/
-
-
 }
