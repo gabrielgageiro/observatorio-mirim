a999fabf04a23df4d7e700df5a88fba2b16220b8
Marcelo Rezin <marcelo.rezin@gmail.com>
2019-12-02 20:32:24 -0300

    Modelagem que armazena a entrada que está sendo efetuada ou que está aguardando sincronização

---
+++ app/src/main/java/com/observatorioMirim/api/models/entrada/EntradaDto.java
@@ -0,0 +1,63 @@
+package com.observatorioMirim.api.models.entrada;
+
+public class EntradaDto {
+
+    private Integer id;
+    private Integer idEscola;
+    private Integer idConta;
+    private Integer idSaida;
+    private String observacao;
+    private boolean finalizada = false;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdEscola() {
+        return idEscola;
+    }
+
+    public void setIdEscola(Integer idEscola) {
+        this.idEscola = idEscola;
+    }
+
+    public Integer getIdConta() {
+        return idConta;
+    }
+
+    public void setIdConta(Integer idConta) {
+        this.idConta = idConta;
+    }
+
+    public Integer getIdSaida() {
+        return idSaida;
+    }
+
+    public void setIdSaida(Integer idSaida) {
+        this.idSaida = idSaida;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public boolean isFinalizada() {
+        return finalizada;
+    }
+
+    public void setFinalizada(boolean finalizada) {
+        this.finalizada = finalizada;
+    }
+
+    public boolean isNew(){
+        return id == null;
+    }
+}
---
+++ app/src/main/java/com/observatorioMirim/api/models/entrada/EntradaDtoDB.java
@@ -0,0 +1,112 @@
+package com.observatorioMirim.api.models.entrada;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public final class EntradaDtoDB extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "observatorio_mirirm";
+    private static final int VERSION = 1;
+
+    private static final String TABELA = "entradas_dto";
+    private static final String COLUNA_ID = "id";
+    private static final String COLUNA_ID_CONTA = "id_conta";
+    private static final String COLUNA_ID_ESCOLA = "id_escola";
+    private static final String COLUNA_ID_SAIDA = "id_saida";
+    private static final String COLUNA_OBSERVACAO = "observacao";
+    private static final String COLUNA_FINALIZADA = "finalizada";
+
+    public EntradaDtoDB(Context context) {
+        super(context, DB_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "CREATE TABLE " + TABELA + " ("
+                + COLUNA_ID + " integer primary key autoincrement,"
+                + COLUNA_ID_CONTA + " integer,"
+                + COLUNA_ID_ESCOLA + " integer,"
+                + COLUNA_ID_SAIDA + " integer,"
+                + COLUNA_OBSERVACAO + " text,"
+                + COLUNA_FINALIZADA + " integer"
+                +")";
+
+        sqLiteDatabase.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABELA);
+        onCreate(sqLiteDatabase);
+    }
+
+    public static int insert(Context context, EntradaDto entradaDto){
+
+        SQLiteDatabase db = new EntradaDtoDB(context).getWritableDatabase();
+        ContentValues valores = new ContentValues();
+        valores.put(COLUNA_ID_CONTA, entradaDto.getIdConta());
+        valores.put(COLUNA_ID_ESCOLA, entradaDto.getIdEscola());
+        valores.put(COLUNA_ID_SAIDA, entradaDto.getIdSaida());
+        valores.put(COLUNA_OBSERVACAO, entradaDto.getObservacao());
+        valores.put(COLUNA_FINALIZADA, entradaDto.isFinalizada());
+        long id = db.insert(TABELA, null, valores);
+        db.close();
+
+        return (int) id;
+    }
+
+    public static void update(Context context, EntradaDto entradaDto){
+        SQLiteDatabase db = new EntradaDtoDB(context).getWritableDatabase();
+
+        String where = COLUNA_ID + " = " + entradaDto.getId();
+
+        ContentValues valores = new ContentValues();
+        valores.put(COLUNA_ID_SAIDA, entradaDto.getIdSaida());
+        valores.put(COLUNA_ID_CONTA, entradaDto.getIdConta());
+        valores.put(COLUNA_ID_ESCOLA, entradaDto.getIdEscola());
+        valores.put(COLUNA_OBSERVACAO, entradaDto.getObservacao());
+        valores.put(COLUNA_FINALIZADA, entradaDto.isFinalizada());
+
+        db.update(TABELA, valores, where,null);
+        db.close();
+    }
+
+    public static void save(Context context, EntradaDto entradaDto){
+        if(entradaDto.isNew()){
+            insert(context, entradaDto);
+        }else {
+            update(context, entradaDto);
+        }
+    }
+
+    public static void delete(Context context, int id){
+        String where = COLUNA_ID + " = " + id;
+
+        SQLiteDatabase db = new EntradaDtoDB(context).getWritableDatabase();
+        db.delete(TABELA, where,null);
+        db.close();
+    }
+
+    public static EntradaDto getBySaida(Context context, int idSaida) {
+
+        SQLiteDatabase db = new EntradaDtoDB(context).getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT  * FROM " + TABELA + " WHERE " + COLUNA_ID_SAIDA + " = " + idSaida, null);
+
+        if (cursor.moveToFirst()) {
+            EntradaDto entradaDto = new EntradaDto();
+            entradaDto.setId(Integer.parseInt(cursor.getString(0)));
+            entradaDto.setIdConta(Integer.parseInt(cursor.getString(1)));
+            entradaDto.setIdEscola(Integer.parseInt(cursor.getString(2)));
+            entradaDto.setIdSaida(Integer.parseInt(cursor.getString(3)));
+            entradaDto.setObservacao(cursor.getString(4));
+            entradaDto.setFinalizada("1".equals(cursor.getString(5)));
+
+            return entradaDto;
+        }
+
+        return null;
+    }
+}
