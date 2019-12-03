7cfc9d6b107bf20f04ab68b728308c0cd4549848
gabrielgageiro <gabrielgageiro@gmail.com>
2019-12-02 23:16:42 -0300

    adicionado o swipe to refresh.

--- app/src/main/java/com/observatorioMirim/views/saida/list/SaidaListFragment.java
+++ app/src/main/java/com/observatorioMirim/views/saida/list/SaidaListFragment.java
@@ -5,9 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.observatorioMirim.R;
 import com.observatorioMirim.api.models.saida.Saida;
@@ -15,26 +17,38 @@ import com.observatorioMirim.api.models.saida.Saida;
 import java.util.ArrayList;
 
 public class SaidaListFragment extends Fragment {
+    SwipeRefreshLayout refreshLayout;
+    ArrayList<Saida> saidas;
+    ListView listaSaida;
 
     @Override
     public View onCreateView(@NonNull final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
 
-        ArrayList<Saida> saidas = (ArrayList<Saida>) getArguments().getSerializable("saidas");
+        saidas = (ArrayList<Saida>) getArguments().getSerializable("saidas");
 
-        if(saidas.isEmpty()){
+        if (saidas.isEmpty()) {
             return inflater.inflate(R.layout.fragment_list_saida_vazio, container, false);
         }
 
         View view = inflater.inflate(R.layout.fragment_list_saida, container, false);
-        ListView listaSaida = view.findViewById(R.id.list_saida);
+        listaSaida = view.findViewById(R.id.list_saida);
 
         SaidaListAdapter saidaListAdapter = new SaidaListAdapter(getActivity(), saidas);
         listaSaida.setAdapter(saidaListAdapter);
 
+        refreshLayout = view.findViewById(R.id.swipe_refresh_saida);
+        refreshLayout.setOnRefreshListener(() -> {
+            Toast.makeText(getContext(), "DEU CERTO SAIDA", Toast.LENGTH_LONG).show();
+            saidas = (ArrayList<Saida>) getArguments().getSerializable("saidas");
+
+            listaSaida.setAdapter(new SaidaListAdapter(getActivity(), saidas));
+            refreshLayout.setRefreshing(false);
+        });
+
         return view;
     }
 
-    public static SaidaListFragment newInstance(final ArrayList<Saida> saidas){
+    public static SaidaListFragment newInstance(final ArrayList<Saida> saidas) {
 
         SaidaListFragment saidaListFragment = new SaidaListFragment();
 
@@ -44,5 +58,4 @@ public class SaidaListFragment extends Fragment {
 
         return saidaListFragment;
     }
-
 }
