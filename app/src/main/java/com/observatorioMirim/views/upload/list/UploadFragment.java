613fc51aa42cfaff7982238bfc9521f933a082c2
Marcelo Rezin <marcelo.rezin@gmail.com>
2019-12-05 19:02:06 -0300

    Melhora no processo de sincronização pela tela de upload

--- app/src/main/java/com/observatorioMirim/views/upload/list/UploadFragment.java
+++ app/src/main/java/com/observatorioMirim/views/upload/list/UploadFragment.java
@@ -6,10 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.observatorioMirim.MainActivity;
 import com.observatorioMirim.R;
 import com.observatorioMirim.api.API;
 import com.observatorioMirim.api.models.RespostaEscola;
@@ -19,6 +21,7 @@ import com.observatorioMirim.api.models.entrada.EntradaDtoDao;
 import com.observatorioMirim.utils.SweetUtils;
 import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
 
+import java.util.Iterator;
 import java.util.List;
 
 import retrofit2.Call;
@@ -46,27 +49,41 @@ public class UploadFragment extends Fragment {
         buttonSincronizar = view.findViewById(R.id.sincronizar);
         buttonSincronizar.setOnClickListener( o -> {
 
+            SweetUtils.loaderNativo(getActivity(), "Sincronizando", "Aguarde enquanto os itens são enviados.");
+
             List<EntradaDto> entradaDtos = EntradaDtoDao.listAllPendentes(getActivity());
 
-            for(EntradaDto e : entradaDtos){
-                Entrada entrada = Entrada.generateEntrada(getActivity(), e);
-
-                API.postEntrada(entrada, new Callback<RespostaEscola>() {
-                    @Override
-                    public void onResponse(Call<RespostaEscola> call, Response<RespostaEscola> response) {
-                        EntradaDtoDao.delete(getActivity(), e.getId());
-                    }
-
-                    @Override
-                    public void onFailure(Call<RespostaEscola> call, Throwable t) {
-                        SweetUtils.message(getActivity(), "Erro", "Não foi possível sincronizar, verifique sua conexão com a internet.", SweetAlertDialog.ERROR_TYPE);
-                    }
-                });
-            }
-
+            enviarEntrada(entradaDtos.iterator());
         });
 
         return view;
+    }
+
+    private void enviarEntrada(Iterator<EntradaDto> iterator){
+
+        EntradaDto entradaDto = iterator.next();
+
+        API.postEntrada(Entrada.generateEntrada(getActivity(), entradaDto), new Callback<RespostaEscola>() {
+            @Override
+            public void onResponse(Call<RespostaEscola> call, Response<RespostaEscola> response) {
+
+                EntradaDtoDao.delete(getActivity(), entradaDto.getId());
+
+                if(iterator.hasNext()){
+                    enviarEntrada(iterator);
+                }else{
+                    SweetUtils.cancelarLoaderNativo();
+                    Toast.makeText(getContext(), "Sincronizado com sucesso!", Toast.LENGTH_LONG).show();
+                    Upload.open((MainActivity) getActivity());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RespostaEscola> call, Throwable t) {
+                SweetUtils.cancelarLoaderNativo();
+                SweetUtils.message(getActivity(), "Erro", "Não foi possível sincronizar, verifique sua conexão com a internet.", SweetAlertDialog.ERROR_TYPE);
+            }
+        });
     }
 
     public static UploadFragment newInstance(){
